b4608085754ca5d56eb093f31467100251299f91
linnaash <allschagieva@gmail.com>
2024-12-05 11:55:15 +0300

    Client_CORS

--- src/Cultura_New/BlazorApp-empty1/Program.cs
+++ src/Cultura_New/BlazorApp-empty1/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://clientforculturawebapi-y5bxc2o4.b4a.run/users") });
 
 await builder.Build().RunAsync();
