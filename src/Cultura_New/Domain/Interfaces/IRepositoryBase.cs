45c089d978f1ac5bdb939bb25ba104abb8b0aae9
linnaash <allschagieva@gmail.com>
2024-12-22 11:23:23 +0300

    IReposotoryBase_проверкаActions

--- src/Cultura_New/Domain/Interfaces/IRepositoryBase.cs
+++ src/Cultura_New/Domain/Interfaces/IRepositoryBase.cs
@@ -6,12 +6,13 @@ namespace Domain.Interfaces
 
     public interface IRepositoryBase<T>
     {
-        Task<List<T>> FindAll();
-        Task<List<T>> FindByCondition(Expression<Func<T, bool>> condition);
-        Task Create(T entity);
-        Task Update(T entity);
-        Task Delete(T entity);
+                        Task<List<T>> FindAll();
+      Task<List<T>> FindByCondition(Expression<Func<T, bool>> condition);
+                                          Task Create(T entity);
+                               Task Update(T entity);
+ Task Delete(T entity);
 
     }
+
 
 }
