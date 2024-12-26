f35900dafaf17426026a1be4aea2b326e8cb8c24
linnaash <allschagieva@gmail.com>
2024-12-26 16:32:29 +0300

    upd

--- src/Cultura_New/BusinessLogic.Tests/obj/Debug/net6.0/BusinessLogic.Tests.AssemblyInfo.cs
+++ src/Cultura_New/BusinessLogic.Tests/obj/Debug/net6.0/BusinessLogic.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLogic.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10abd354994668e70f6806c89832d126114faf58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79515083519fe0a9983f625baac94bf528538a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLogic.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLogic.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
