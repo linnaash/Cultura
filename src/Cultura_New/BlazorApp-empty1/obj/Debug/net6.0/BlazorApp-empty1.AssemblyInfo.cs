1a0461ab7316456e3c4c09cd11a331758f952dde
linnaash <allschagieva@gmail.com>
2024-12-26 16:31:57 +0300

    upd

--- src/Cultura_New/BlazorApp-empty1/obj/Debug/net6.0/BlazorApp-empty1.AssemblyInfo.cs
+++ src/Cultura_New/BlazorApp-empty1/obj/Debug/net6.0/BlazorApp-empty1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp-empty1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef4f21985e7658875a75b284d6b07ad094e13c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79515083519fe0a9983f625baac94bf528538a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp-empty1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp-empty1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
